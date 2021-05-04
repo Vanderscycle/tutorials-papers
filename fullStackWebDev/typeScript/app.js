e3eca296d52ae86a760bb4618f3e994365f62c7c
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-05-03 18:30:00 -0700

    learned more about JS/TS.

--- fullStackWebDev/typeScript/app.js
+++ fullStackWebDev/typeScript/app.js
@@ -1,1 +1,15 @@
 console.log("Your code goes here...!!!");
+function add(n1, n2, showRes) {
+    console.log(typeof number1);
+    if (showRes) {
+        console.log(n1 + n2);
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 5; //'5'+2.8=52.8 
+var number2 = 2.8;
+var printResults = true;
+var result = add(number1, number2, printResults);
+console.log(result);
--- fullStackWebDev/typeScript/app.ts
+++ fullStackWebDev/typeScript/app.ts
@@ -1,2 +1,14 @@
 console.log("Your code goes here...!!!");
-
+function add(n1: number, n2: number, showRes: Boolean) {
+    console.log(typeof number1);
+    if (showRes) {
+        console.log(n1 + n2);
+    } else {
+        return n1 + n2;
+    }
+}
+const number1 = 5; //'5'+2.8=52.8
+const number2 = 2.8;
+const printResults = true;
+const result = add(number1, number2, printResults);
+console.log(result);
