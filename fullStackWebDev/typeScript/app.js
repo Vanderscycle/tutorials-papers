89c719b4694a9899d72e94681729de8ae3c9f2fe
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-05-14 20:07:34 -0700

    tiny progress done toward learning the basic of typeScript.

--- fullStackWebDev/typeScript/app.js
+++ fullStackWebDev/typeScript/app.js
@@ -9,3 +9,5 @@ function printResult(num) {
 printResult(add(5, 12));
 var combineValues = add; //pointer to a function
 console.log(combineValues(8, 8));
+// TODO hello 
+
--- fullStackWebDev/typeScript/app.ts
+++ fullStackWebDev/typeScript/app.ts
@@ -15,7 +15,6 @@ function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
     const result = n1 + n2;
     cb(result);
 }
-a
 printResult(add(5, 12));
 
 let combineValues: (a: number, b: number) => number; //Can take anyfunction that has two input as number
@@ -28,4 +27,3 @@ console.log(combineValues(8, 8));
 addAndHandle(10, 20, (result) => {
     console.log(result); //result is an anonymous function (lambda)
 });
-sd
