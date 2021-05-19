101b5c3ccb4f0160dfa165ca79fd92dfa4b6df3f
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-05-18 21:14:17 -0700

    graphql is cool but difficult to set

--- fullStackWebDev/benAwadProject/src/entities/post.ts
+++ fullStackWebDev/benAwadProject/src/entities/post.ts
@@ -1,17 +1,26 @@
 //https://mikro-orm.io/docs/defining-entities
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { ObjectType, Field, Int} from "type-graphql" // because we need to set the type for some fields (date, ints) we import 
+// WARN: Int comes from typegraphql but string doesn't ?
 
+@ObjectType() // we can stack decorators
 @Entity()
 export class Post {
+
+    @Field(() => Int) // to add a field to each ones
     @PrimaryKey()
     id!: number;
 
+    @Field(() => String) // WARN: if there is a field I do not want to expose then  do not add Field decorator
     @Property() // {type: "date"}
     createdAt: Date = new Date();
+    
 
+    @Field(() => String) // 
     @Property({ onUpdate: () => new Date() })
     updatedAt: Date = new Date();
 
+    @Field()
     @Property({type: 'text'})
     title!: string;
 }
