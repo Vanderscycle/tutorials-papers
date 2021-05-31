f8e1f29b1136686005d9b57f5731edc0e8c98b13
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-05-31 12:58:00 -0700

    made progress w/ the basic structure of svelte. must debug the conditionals.

--- fullStackWebDev/svelteJestTS/src/App.svelte
+++ fullStackWebDev/svelteJestTS/src/App.svelte
@@ -1,30 +1,52 @@
 <script lang="ts">
-	export let name: string;
+    //export let name: string;
+    let name: string = "bro McChad";
+    let points: number = 100;
+    const addPoint = () => {
+        points += 1;
+    };
+    const removePoint = () => {
+        points -= 1;
+    };
+    const showControls: boolean = false
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    h3 {
+        margin-bottom: 10 px;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
+
+<main>
+    <div class="container">
+        <div class="card">
+            <h1>{name}</h1>
+            <h3>{points}</h3>
+            {#if showControls}
+                <button class="btn" on:click={addPoint}>+1</button>
+                <button class="btn btn-dark" on:click={removePoint}>-1</button>
+                <input type="number" bind:value={points} />
+            {/if}
+        </div>
+    </div>
+</main>
