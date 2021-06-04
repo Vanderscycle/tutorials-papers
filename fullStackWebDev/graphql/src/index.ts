77f90b7d21c7aae81301efea3a600e18a0faad74
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-06-03 19:37:17 -0700

    found duplicates and badly named folders.

--- fullStackWebDev/graphql/src/index.ts
+++ fullStackWebDev/graphql/src/index.ts
@@ -1,1 +1,18 @@
-    console.log("Hello World, don't forget to inspect when debugging")
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {User} from "./entity/User";
+
+createConnection().then(async connection => {
+
+    console.log("Inserting a new user into the database...");
+    const user = new User();
+
+    user.name = "Timber";
+    user.email = "chad@chad";
+    user.role = 'admin';
+
+    await user.save;
+
+    console.log("User Created");
+
+}).catch(error => console.log(error));
