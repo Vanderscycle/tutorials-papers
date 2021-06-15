de3df151b4edf9cd775becd36698ce6b123f1751
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-06-15 11:13:34 -0700

    vue is able to fetch the data from jsonplaceholder. Meaning to connect the front to the backend I can run graphql on an express server and have vue connect to it.

--- fullStackWebDev/vueX/src/components/Todos.vue
+++ fullStackWebDev/vueX/src/components/Todos.vue
@@ -1,16 +1,38 @@
 <template>
-  <div>
-    <h3>Todos</h3>
+  <h3>Todos</h3>
+  <div class="todos">
     <div v-for="todo in allTodos" v-bind:key="todo.id" class="todo">
-    {{ todo.title }}
+      {{ todo.title }}
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters, mapActions } from "vuex";
 export default {
   name: "Todos",
-  computed: mapGetters(['allTodos'])
+  methods: {
+    ...mapActions(["fetchTodos"]),
+  },
+  computed: mapGetters(["allTodos"]),
+  created() {
+    this.fetchTodos();
+  },
 };
 </script>
+<style scoped>
+.todos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+}
+.todo {
+  border: 1px solid #ccc;
+  background: #41b883;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
+</style>
--- fullStackWebDev/vueX/src/store/modules/todos.ts
+++ fullStackWebDev/vueX/src/store/modules/todos.ts
@@ -1,25 +1,24 @@
 import axios from 'axios'
 
 const state = {
-  todos: [
-    {
-      id: 1,
-      title: 'Todo One'
-    },
-    {
-      id: 2,
-      title: 'Todo dos'
-    }
-  ]
+  todos: []
 }
 
 const getters = {
   allTodos:(state) => state.todos
 }
 
-const actions = {};
+const actions = {
+  //we don't call the mutation directly
+  async fetchTodos({ commit }) {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
+    commit('setTodos',response.data)
+  }
+};
 
-const mutations = {}
+const mutations = {
+  setTodos: (state, todos) => (state.todos = todos)
+}
 
 export default {
   state,
