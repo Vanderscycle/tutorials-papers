a9d2ac9dac39c73c0144ccce98565c259563794b
Henri Vandersleyen <hvandersleyen@gmail.com>
2022-01-31 19:44:28 -0800

    I am starting to like rust

--- learningRust/src/main.rs
+++ learningRust/src/main.rs
@@ -2,9 +2,11 @@
 
 // mod print;
 // mod vars;
-mod types;
+// mod types;
+mod strings;
 fn main() {
     // print::run()
     // vars::run()
-    types::run()
+    // types::run()
+    strings::run()
 }
